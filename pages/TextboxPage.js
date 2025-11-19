6013d9e5004b051342bed9f10069a98af865e30e
Arina Avlasova <avlasva@mail.ru>
2025-11-19 18:41:01 +0300

    new element created

--- pages/TextboxPage.js
+++ pages/TextboxPage.js
@@ -5,19 +5,34 @@ export class TextboxPage extends BasePage{
         this.FullNameBox = page.locator ('#userName');
         this.EmailBox = page.locator ('#userEmail');
         this.CurrentAddressBox = page.locator ('#currentAddress');
-        this.PermanentAdressBox = page.locator ('#permanentAddress');
+        this.PermanentAddressBox = page.locator ('#permanentAddress');
         this.SubmitButton = page.locator ('#submit');
         this.NavigationBarButton = page.locator ('.navbar-toggler-icon');
+        this.OutputBox = page.locator('#output');
     };
     async open() {
         await this.NavigateTo ('/text-box');
     };
-    async FillUserInfo (fullName, email, currentAdress, permAdress) {
-        await this.FullNameBox.fill (fullName);
-        await this.EmailBox.fill (email);
-        await this.CurrentAddressBox.fill (currentAdress);
-        await this.PermanentAdressBox.fill (permAdress);
-    };
+    async FillUserInfo (userInfo) {
+        if (userInfo.fullName) {
+            await this.FullNameBox.fill (userInfo.fullName);
+        }
+        if (userInfo.email) {
+            await this.EmailBox.fill (userInfo.email);
+        }
+        if (userInfo.currentAddress) {
+            await this.CurrentAddressBox.fill (userInfo.currentAddress);
+        }
+        if (userInfo.permanentAddress) {
+            await this.PermanentAddressBox.fill (userInfo.permanentAddress);
+        }
+    }
+//    async FillUserInfo (fullName, email, currentAdress, permAdress) {
+//        await this.FullNameBox.fill (fullName);
+//        await this.EmailBox.fill (email);
+//        await this.CurrentAddressBox.fill (currentAdress);
+//        await this.PermanentAddressBox.fill (permAdress);
+//    };
     async ClickSubmit () {
         await this.SubmitButton.click();
     }
