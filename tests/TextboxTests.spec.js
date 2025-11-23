25bf21ce03da30f4463ce84e86d20346a4b36ae9
Arina Avlasova <avlasva@mail.ru>
2025-11-23 09:38:05 +0300

    new tests

--- tests/TextboxTests.spec.js
+++ tests/TextboxTests.spec.js
@@ -2,7 +2,7 @@ import {test, expect} from '@playwright/test';
 import { BasePage } from '../pages/BasePage';
 import { TextboxPage } from '../pages/TextboxPage';
 
-test ('submit form with valid data @ui', async ({page}) => {
+test ('submit form with valid data @pos', async ({page}) => {
     const textboxPage = new TextboxPage (page);
     await textboxPage.open();
     await textboxPage.FillUserInfo({
@@ -19,7 +19,7 @@ test ('submit form with valid data @ui', async ({page}) => {
     await expect(page.getByText('Permananet Address :New York, secondExample Street')).toBeVisible();
 }); 
 
-test ('entering special sybmols into Full Name, Current/Permanent Address fields', async({page}) => {
+test ('entering special sybmols into Full Name, Current/Permanent Address fields @pos', async({page}) => {
     const textboxPage = new TextboxPage(page);
     await textboxPage.open();
     await textboxPage.FillUserInfo({
@@ -34,7 +34,7 @@ test ('entering special sybmols into Full Name, Current/Permanent Address fields
     await expect(page.getByText('Permananet Address :New York, street Avenue 16/86-2')).toBeVisible();
 });
 
-test ('Submission the form with uppercase case values', async({page}) => {
+test ('Submission the form with uppercase case values @pos', async({page}) => {
     const textboxPage = new TextboxPage(page);
     await textboxPage.open();
     await textboxPage.FillUserInfo({
@@ -50,7 +50,7 @@ test ('Submission the form with uppercase case values', async({page}) => {
     await expect(page.getByText('Permananet Address :')).not.toBeVisible();
 });
 
-test ('Fulfilling the "Current Adrdess" field with long text value', async({page}) => {
+test ('Fulfilling the "Current Adrdess" field with long text value @pos', async({page}) => {
     const textboxPage = new TextboxPage(page);
     await textboxPage.open();
     await textboxPage.FillUserInfo({ 
@@ -78,5 +78,47 @@ test ('email field validation with invalid data @neg',async({page}) => {
     await expect(textboxPage.EmailBox).toHaveClass('mr-sm-2 field-error form-control');
     await expect(page.getByText('Current Address :London')).not.toBeVisible();
     await expect(page.getByText('Permananet Address :New York')).not.toBeVisible();
+});
+
+test ('Form submission with empty "Full Name" field @neg', async ({page})=> {
+    const textboxPage = new TextboxPage(page);
+    await textboxPage.open()
+    await textboxPage.FillUserInfo({
+        email:"example@gmail.com",
+        currentAddress:"London",
+        permanentAddress:"New York"
+    });
+    await textboxPage.ClickSubmit();
+
+    await expect(page.locator('#name')).not.toBeVisible();
+    await expect(page.getByText("Email:example@gmail.com")).toBeVisible();
+    await expect(page.getByText('Current Address :London')).toBeVisible();
+    await expect(page.getByText('Permananet Address :New York')).toBeVisible();
+});
+
+test('Entering special symbols into "Email" field @neg', async ({page}) => {
+    const textboxPage = new TextboxPage(page);
+    await textboxPage.open();
+    await textboxPage.FillUserInfo({
+        email:"example!@gmail.com"
+    })
+    await textboxPage.ClickSubmit();
+
+    await expect(textboxPage.EmailBox).toHaveClass('mr-sm-2 field-error form-control');
+    await expect(page.locator('#output')).not.toBeVisible();
+});
+
+test('Fullfilling the form with long text input @ui', async ({page}) => {
+    const textboxPage = new TextboxPage(page);
+    await textboxPage.open();
+    const longAddress = "Example".repeat(17);
+    await textboxPage.FillUserInfo({
+        fullName :"John Doe",
+        currentAddress: longAddress
+    });
+    await textboxPage.ClickSubmit();
+
+    await expect(page.getByText('Name:John Doe')).toBeVisible();
+    await textboxPage.CheckCurrAddressInsideOutput(longAddress);
 });
 
