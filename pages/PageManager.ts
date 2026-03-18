87f60ce4b0d6d7945966ac648b7689e3cf737aed
avlasova05 <avlasva@mail.ru>
2026-03-18 09:08:27 +0100

    updates

--- pages/PageManager.ts
+++ pages/PageManager.ts
@@ -1,22 +1,22 @@
 import { Page } from "@playwright/test";
 import { CheckboxPage } from "./CheckboxPage";
-import { RadioButtonPage } from "./RadioButtonPage";
+
 import { TextboxPage } from "./TextboxPage";
-import { WebTablePage } from "./WebTablePage";
+//import { WebTablePage } from "./WebTablePage";
 import { BasePage } from "./BasePage";
 
 export class PageManager {
     page: Page;
     checkboxPage: CheckboxPage;
-    radiobuttonPage : RadioButtonPage;
+    //radiobuttonPage : RadioButtonPage;
     textboxPage : TextboxPage;
-    webtablePage: WebTablePage;
+   // webtablePage: WebTablePage;
 
     constructor(page:Page) {
         this.page = page;
         this.checkboxPage = new CheckboxPage(page);
-        this.radiobuttonPage = new RadioButtonPage(page);
+        //this.radiobuttonPage = new RadioButtonPage(page);
         this.textboxPage = new TextboxPage(page);
-        this.webtablePage = new WebTablePage(page);
+        //this.webtablePage = new WebTablePage(page);
     }
 }
